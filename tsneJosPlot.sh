44a0a69ebd1ef86e4e386cd960f4537461dcbb05
jos <jos.v.roosmalen@gmail.com>
2017-01-08 11:16:57 +0100

    Axis change

--- tsneJosPlot.sh
+++ tsneJosPlot.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
+python3 tsneJosPlot.py ~/masterRepo/Thesis2/data/output_2D.dat /home/josr/masterRepo/Thesis2/vectorGraphics/tsne_output_2D.pdf
 
-./tsneJosPlot.py ~/masterRepo/masterRepo/Thesis/data/tsne_4158D-400p.dat /home/josr/masterRepo/masterRepo/Thesis/vectorGraphics/tsne_4158D-400p.pdf
-./tsneJosPlot.py ~/masterRepo/masterRepo/Thesis/data/tsne_5000D-400p.dat /home/josr/masterRepo/masterRepo/Thesis/vectorGraphics/tsne_5000D-400p.pdf
-./tsneJosPlot.py ~/masterRepo/masterRepo/Thesis/data/tsne_3000D-400p.dat /home/josr/masterRepo/masterRepo/Thesis/vectorGraphics/tsne_3000D-400p.pdf
-./tsneJosPlot.py ~/masterRepo/masterRepo/Thesis/data/tsne_1500D-400p.dat /home/josr/masterRepo/masterRepo/Thesis/vectorGraphics/tsne_1500D-400p.pdf
-./tsneJosPlot.py ~/masterRepo/masterRepo/Thesis/data/output_2D.dat /home/josr/masterRepo/masterRepo/Thesis/vectorGraphics/tsne_output_2D.pdf
-
-gnome-open /home/josr/masterRepo/masterRepo/Thesis/vectorGraphics/tsne_output_2D.pdf
+gnome-open /home/josr/masterRepo/Thesis2/vectorGraphics/tsne_output_2D.pdf
